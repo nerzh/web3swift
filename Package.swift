1f9406b442ae9e58497debca29b582eb521d4eee
OleG <emptystamp@gmail.com>
2020-09-26 13:23:50 +0300

    aa

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
   name: "Web3swift",
+  platforms: [
+      .iOS(.v9)
+  ],
   products: [
     // Products define the executables and libraries produced by a package, and make them visible to other packages.
     .library(name: "web3swift", targets: ["web3swift"]),
